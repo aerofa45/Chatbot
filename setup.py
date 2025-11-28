4e417f50510dd34e88ff0edad518f6b6875ceaf9
aerofa45 <aerofa45@gmail.com>
2025-11-27 19:36:20 -0600

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical chatbot",
+    version="0.1.0",
+    author="Aman",
+    author_email="achaudhary11s@semo.edu",
+    packages=find_packages(),
+    install_requires=[]
+)
